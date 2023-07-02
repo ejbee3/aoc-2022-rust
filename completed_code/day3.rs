e69b01b5132c86369c7c3a313b3e8eb458e538ae
jo bars <jobars@protonmail.com>
2023-07-01 20:00:09 -0400

    completed day 2 and day 3

---
+++ completed_code/day3.rs
@@ -0,0 +1,92 @@
+use itertools::Itertools;
+use std::fs;
+
+fn main() {
+    // let d = 'D';
+    // let d_i = d as u32;
+
+    // println!("{:?}", d_i);
+    // 68
+
+    let contents = fs::read_to_string("input.txt").expect("wrong file name!");
+    // let mut priorities: Vec<u8> = vec![];
+
+    // for line in contents.lines() {
+    //     let halfway = line.len() / 2;
+    //     let first_half = &line[0..halfway];
+    //     let second_half = &line[halfway..line.len()];
+
+    //     let vec_first = first_half.as_bytes();
+    //     let vec_second = second_half.as_bytes();
+    //     let mut matching_byte: u8 = 0;
+
+    //     for a in vec_first.into_iter().cartesian_product(vec_second) {
+    //         if a.0 == a.1 {
+    //             matching_byte = *a.0;
+    //             break;
+    //         }
+    //     }
+
+    //     if matching_byte >= 65 && matching_byte <= 90 {
+    //         let upper_prio = matching_byte - 38;
+    //         priorities.push(upper_prio);
+    //     } else {
+    //         let lower_prio = matching_byte - 96;
+    //         priorities.push(lower_prio);
+    //     }
+    // }
+
+    // let priorities_sum = priorities.iter().fold(0u32, |sum, i| sum + (*i as u32));
+
+    // let matching_char = matching_byte as char;
+
+    // println!("{:?}", priorities_sum);
+    // println!("{:?}", vec_b);
+
+    let mut priorities: Vec<u8> = vec![];
+    let mut matching_byte: u8 = 0;
+    let mut threes: Vec<Vec<u8>> = vec![];
+    let mut elves_bytes: Vec<Vec<Vec<u8>>> = vec![];
+
+    for (i, line) in contents.lines().into_iter().enumerate() {
+        if i % 3 == 0 && i != 0 {
+            elves_bytes.push(threes.clone());
+            threes.clear();
+            threes.push(line.to_string().into_bytes());
+        } else if i == 299 {
+            threes.push(line.to_string().into_bytes());
+            elves_bytes.push(threes.clone());
+        } else {
+            threes.push(line.to_string().into_bytes());
+        }
+    }
+
+    for t in &elves_bytes {
+        let mut matching_bytes: Vec<u8> = vec![];
+        for a in t[0].clone().into_iter().cartesian_product(&t[1]) {
+            if a.0 == *a.1 {
+                matching_bytes.push(a.0);
+            }
+        }
+
+        for b in matching_bytes.into_iter().cartesian_product(&t[2]) {
+            if b.0 == *b.1 {
+                matching_byte = b.0;
+            }
+        }
+
+        if matching_byte >= 65 && matching_byte <= 90 {
+            let upper_prio = matching_byte - 38;
+            priorities.push(upper_prio);
+        } else {
+            let lower_prio = matching_byte - 96;
+            priorities.push(lower_prio);
+        }
+
+        matching_byte = 0;
+    }
+
+    let priorities_sum = priorities.iter().fold(0u32, |sum, i| sum + (*i as u32));
+
+    println!("{:?}", priorities_sum);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,50 @@
-use itertools::Itertools;
 use std::fs;
 
 fn main() {
-    // let d = 'D';
-    // let d_i = d as u32;
+    let contents = fs::read_to_string("input2.txt").expect("wrong file name!");
+    let mut totals: Vec<u8> = vec![];
 
-    // println!("{:?}", d_i);
-    // 68
-
-    let contents = fs::read_to_string("input.txt").expect("wrong file name!");
-    // let mut priorities: Vec<u8> = vec![];
-
-    // for line in contents.lines() {
-    //     let halfway = line.len() / 2;
-    //     let first_half = &line[0..halfway];
-    //     let second_half = &line[halfway..line.len()];
-
-    //     let vec_first = first_half.as_bytes();
-    //     let vec_second = second_half.as_bytes();
-    //     let mut matching_byte: u8 = 0;
-
-    //     for a in vec_first.into_iter().cartesian_product(vec_second) {
-    //         if a.0 == a.1 {
-    //             matching_byte = *a.0;
-    //             break;
-    //         }
-    //     }
-
-    //     if matching_byte >= 65 && matching_byte <= 90 {
-    //         let upper_prio = matching_byte - 38;
-    //         priorities.push(upper_prio);
-    //     } else {
-    //         let lower_prio = matching_byte - 96;
-    //         priorities.push(lower_prio);
-    //     }
-    // }
-
-    // let priorities_sum = priorities.iter().fold(0u32, |sum, i| sum + (*i as u32));
-
-    // let matching_char = matching_byte as char;
-
-    // println!("{:?}", priorities_sum);
-    // println!("{:?}", vec_b);
-
-    let mut priorities: Vec<u8> = vec![];
-    let mut matching_byte: u8 = 0;
-    let mut threes: Vec<Vec<u8>> = vec![];
-    let mut elves_bytes: Vec<Vec<Vec<u8>>> = vec![];
-
-    let mut elves: Vec<Vec<&str>> = vec![];
-    let mut threes_str: Vec<&str> = vec![];
-
-    for (i, line) in contents.lines().into_iter().enumerate() {
-        if i % 3 == 0 && i != 0 {
-            // elves_bytes.push(threes.clone());
-            elves.push(threes_str.clone());
-
-            // threes.clear();
-            threes_str.clear();
-
-            // threes.push(line.to_string().into_bytes());
-            threes_str.push(line);
-        } else {
-            // threes.push(line.to_string().into_bytes());
-            threes_str.push(line);
-        }
+    for line in contents.lines() {
+        let round_total = get_round_total_two(line);
+        totals.push(round_total);
     }
 
-    // for t in &elves_bytes {
-    //     let mut matching_bytes: Vec<u8> = vec![];
-    //     for a in t[0].clone().into_iter().cartesian_product(&t[1]) {
-    //         if a.0 == *a.1 {
-    //             matching_bytes.push(a.0);
-    //         }
-    //     }
+    // println!("{:?}", totals);
+    let all_rounds_total = totals.iter().fold(0u32, |sum, i| sum + (*i as u32));
 
-    //     for b in matching_bytes.into_iter().cartesian_product(&t[2]) {
-    //         if b.0 == *b.1 {
-    //             matching_byte = b.0;
-    //         }
-    //     }
+    println!("{}", all_rounds_total);
+}
 
-    //     if matching_byte >= 65 && matching_byte <= 90 {
-    //         let upper_prio = matching_byte - 38;
-    //         priorities.push(upper_prio);
-    //     } else {
-    //         let lower_prio = matching_byte - 96;
-    //         priorities.push(lower_prio);
-    //     }
+// fn get_round_total_one(ln: &str) -> u8 {
+//     let total = match ln {
+//         "B Z" => 9,
+//         "A Y" => 8,
+//         "C X" => 7,
+//         "C Z" => 6,
+//         "B Y" => 5,
+//         "A X" => 4,
+//         "A Z" => 3,
+//         "C Y" => 2,
+//         "B X" => 1,
+//         _ => 0,
+//     };
 
-    //     matching_byte = 0;
-    // }
+//     total
+// }
 
-    // let priorities_sum = priorities.iter().fold(0u32, |sum, i| sum + (*i as u32));
+fn get_round_total_two(ln: &str) -> u8 {
+    let total = match ln {
+        "B Z" => 9,
+        "A Y" => 4,
+        "C X" => 2,
+        "C Z" => 7,
+        "B Y" => 5,
+        "A X" => 3,
+        "A Z" => 8,
+        "C Y" => 6,
+        "B X" => 1,
+        _ => 0,
+    };
 
-    println!("{:?}", elves);
+    total
 }
