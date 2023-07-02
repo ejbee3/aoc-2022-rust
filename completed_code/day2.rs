b4de3395b841aea223394a4284988f9d81fce113
jo bars <jobars@protonmail.com>
2023-07-02 05:05:29 -0400

    day 4 part one done

--- completed_code/day2.rs
+++ completed_code/day2.rs
@@ -1,11 +1,11 @@
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("input.txt").expect("wrong file name!");
+    let contents = fs::read_to_string("input2.txt").expect("wrong file name!");
     let mut totals: Vec<u8> = vec![];
 
     for line in contents.lines() {
-        let round_total = get_round_total(line);
+        let round_total = get_round_total_two(line);
         totals.push(round_total);
     }
 
@@ -15,16 +15,33 @@ fn main() {
     println!("{}", all_rounds_total);
 }
 
-fn get_round_total(ln: &str) -> u8 {
+// fn get_round_total_one(ln: &str) -> u8 {
+//     let total = match ln {
+//         "B Z" => 9,
+//         "A Y" => 8,
+//         "C X" => 7,
+//         "C Z" => 6,
+//         "B Y" => 5,
+//         "A X" => 4,
+//         "A Z" => 3,
+//         "C Y" => 2,
+//         "B X" => 1,
+//         _ => 0,
+//     };
+
+//     total
+// }
+
+fn get_round_total_two(ln: &str) -> u8 {
     let total = match ln {
         "B Z" => 9,
-        "A Y" => 8,
-        "C X" => 7,
-        "C Z" => 6,
+        "A Y" => 4,
+        "C X" => 2,
+        "C Z" => 7,
         "B Y" => 5,
-        "A X" => 4,
-        "A Z" => 3,
-        "C Y" => 2,
+        "A X" => 3,
+        "A Z" => 8,
+        "C Y" => 6,
         "B X" => 1,
         _ => 0,
     };
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,38 @@
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("input2.txt").expect("wrong file name!");
-    let mut totals: Vec<u8> = vec![];
+    let contents = fs::read_to_string("input.txt").expect("wrong file name!");
+
+    let mut count: u32 = 0;
 
     for line in contents.lines() {
-        let round_total = get_round_total_two(line);
-        totals.push(round_total);
+        let split = line.split("-").collect::<Vec<&str>>();
+        // println!("{:?}", split);
+
+        let middle = split[1].split(",").collect::<Vec<&str>>();
+        // println!("{:?}", middle);
+
+        let first_pair = (num_parser(split[0])..num_parser(middle[0]) + 1).collect::<Vec<u32>>();
+        // println!("{:?}", first_pair);
+
+        let second_pair = (num_parser(middle[1])..num_parser(split[2]) + 1).collect::<Vec<u32>>();
+        // println!("{:?}", second_pair);
+
+        if first_pair.len() > second_pair.len()
+            && second_pair[0] >= first_pair[0]
+            && second_pair[second_pair.len() - 1] <= first_pair[first_pair.len() - 1]
+        {
+            count += 1;
+        } else if first_pair[0] >= second_pair[0]
+            && first_pair[first_pair.len() - 1] <= second_pair[second_pair.len() - 1]
+        {
+            count += 1;
+        }
     }
 
-    // println!("{:?}", totals);
-    let all_rounds_total = totals.iter().fold(0u32, |sum, i| sum + (*i as u32));
-
-    println!("{}", all_rounds_total);
+    println!("{:?}", count);
 }
 
-// fn get_round_total_one(ln: &str) -> u8 {
-//     let total = match ln {
-//         "B Z" => 9,
-//         "A Y" => 8,
-//         "C X" => 7,
-//         "C Z" => 6,
-//         "B Y" => 5,
-//         "A X" => 4,
-//         "A Z" => 3,
-//         "C Y" => 2,
-//         "B X" => 1,
-//         _ => 0,
-//     };
-
-//     total
-// }
-
-fn get_round_total_two(ln: &str) -> u8 {
-    let total = match ln {
-        "B Z" => 9,
-        "A Y" => 4,
-        "C X" => 2,
-        "C Z" => 7,
-        "B Y" => 5,
-        "A X" => 3,
-        "A Z" => 8,
-        "C Y" => 6,
-        "B X" => 1,
-        _ => 0,
-    };
-
-    total
+fn num_parser(s: &str) -> u32 {
+    s.parse().expect("this should be a number from the input!")
 }
